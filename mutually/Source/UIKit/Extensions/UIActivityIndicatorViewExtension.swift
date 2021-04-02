c1c7270d7fbb8aaac40d69735f7d1f3e634acdd8
Turan Assylkhan <turan@strongte.am>
2021-04-02 17:41:04 +0600

    feat: confirm to indicator protocol

---
+++ mutually/Source/UIKit/Extensions/UIActivityIndicatorViewExtension.swift
@@ -0,0 +1,29 @@
+//
+//  UIActivityIndicatorViewExtension.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 02.04.2021.
+//
+
+import UIKit
+
+extension UIActivityIndicatorView: IndicatorProtocol {
+    public var radius: CGFloat {
+        get { frame.width / 2 }
+        set {
+            frame.size = CGSize(width: 2 * newValue, height: 2 * newValue)
+            setNeedsDisplay()
+        }
+    }
+    
+    public var color: UIColor {
+        get {
+            return tintColor
+        }
+        set {
+            tintColor = newValue
+        }
+    }
+    // unused
+    public func setupAnimation(in layer: CALayer, size: CGSize) {}
+}
