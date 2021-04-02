10c41808b65cce3ba4b6a0fbda5728fa82144f41
Turan Assylkhan <turan@strongte.am>
2021-04-02 17:40:12 +0600

    feat: button init

---
+++ mutually/Source/UIKit/Buttons/PrimaryButton.swift
@@ -0,0 +1,28 @@
+//
+//  PrimaryButton.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 02.04.2021.
+//
+
+import UIKit
+
+extension Button {
+    static func makePrimary(with text: String? = nil) -> Button {
+        let button = Button(text: text,
+                            textStyle: .buttonPrimary,
+                            textColor: .white,
+                            backgroundColor: Color.buttonPrimaryFillRegular,
+                            pressedColor: Color.buttonPrimaryFillActive,
+                            disabledColor: Color.buttonPrimaryFillRegular,
+                            cornerRadius: 8,
+                            withShadow: false)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        let indicatorView = UIActivityIndicatorView()
+        indicatorView.style = .white
+        indicatorView.color = .white
+        button.indicator = indicatorView
+        return button
+    }
+}
