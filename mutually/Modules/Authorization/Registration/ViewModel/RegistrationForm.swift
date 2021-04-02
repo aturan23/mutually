ef46fdc2b57af7abcf725cc3c3b0c2f86e4cb3a7
Turan Assylkhan <turan@strongte.am>
2021-04-02 21:11:56 +0600

    feat: registration form

---
+++ mutually/Modules/Authorization/Registration/ViewModel/RegistrationForm.swift
@@ -0,0 +1,12 @@
+//
+//  RegistrationForm.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 02.04.2021.
+//
+
+struct RegistrationForm {
+    var phone: String? = nil
+    var dataProcessingSelected = false
+    var termsSelected = false
+}
