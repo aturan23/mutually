659c25dd99927d552b4b244851930006e0105ea3
Turan Assylkhan <turan@strongte.am>
2021-04-03 19:33:45 +0600

    feat: setup keyboard view

--- mutually/Modules/Authorization/SetupPasscode/View/SetupPasscodeViewController.swift
+++ mutually/Modules/Authorization/SetupPasscode/View/SetupPasscodeViewController.swift
@@ -19,6 +19,14 @@ class SetupPasscodeViewController: BaseViewController, SetupPasscodeViewInput {
     // ------------------------------
     // MARK: - UI components
     // ------------------------------
+    
+    private let labelFactory = LabelFactory()
+    
+    private lazy var keyboardView: PasswordKeyboardView = {
+        let view = PasswordKeyboardView()
+        view.output = self
+        return view
+    }()
 
     // ------------------------------
     // MARK: - Life cycle
@@ -48,10 +56,36 @@ class SetupPasscodeViewController: BaseViewController, SetupPasscodeViewInput {
     }
 
     private func setupViewsHierarchy() {
-
+        [keyboardView].forEach(view.addSubview(_:))
     }
 
     private func setupConstraints() {
+        var viewSafeAreaHeight: CGFloat = view.bounds.height
+        if let windowInsets = UIApplication.shared.keyWindow?.safeAreaInsets {
+            viewSafeAreaHeight -= windowInsets.top + windowInsets.bottom
+        }
         
+        keyboardView.snp.makeConstraints {
+            $0.height.equalTo(viewSafeAreaHeight / 2)
+            $0.width.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaInsets.bottom)
+        }
+    }
+}
+
+// ------------------------------
+// MARK: - PasswordKeyboardViewOutput
+// ------------------------------
+
+extension SetupPasscodeViewController: PasswordKeyboardViewOutput {
+    func didPressButton(type: PasswordKeyboardButtonType) {
+//        switch type {
+//        case .digit(let value):
+//            passwordViewInput.append(symbol: value)
+//        case .backspace:
+//            passwordViewInput.eraseLastSymbol()
+//        case .biometry:
+//            break
+//        }
     }
 }
