188ced204242fd50cf93d95f6f9fc59338e3c667
Turan Assylkhan <turan@strongte.am>
2021-04-03 08:09:19 +0600

    feat: navigation item extension

---
+++ mutually/Source/UIKit/Extensions/UINavigationItemExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UINavigationItemExtension.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 03.04.2021.
+//
+
+import UIKit
+
+extension UINavigationItem {
+    func setBackButtonItem() {
+        backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
