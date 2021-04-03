9c1718418c53a593197fd93e0b467659265d953f
Turan Assylkhan <turan@strongte.am>
2021-04-03 09:03:49 +0600

    feat: sms verification state & replace smsverificationpurpose

---
+++ mutually/Modules/Authorization/SmsVerification/Entity/SmsVerificationViewState.swift
@@ -0,0 +1,13 @@
+//
+//  SmsVerificationViewState.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 03.04.2021.
+//
+
+enum SmsVerificationViewState {
+    case requestingCode
+    case inputCode
+    case requestNewCode
+    case validatingCode
+}
