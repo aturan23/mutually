16849107e0b75e74f90e831606292abf0ef28c65
Turan Assylkhan <turan@strongte.am>
2021-04-03 07:32:30 +0600

    feat: logout on session expired removed

--- mutually/Core/Services/Session/SessionTracker.swift
+++ mutually/Core/Services/Session/SessionTracker.swift
@@ -20,9 +20,6 @@ final class SessionTracker: SessionTrackerProtocol {
     private let rootController: RootUIControllerType
     private let userDefaults: UserDefaults?
     private let authorizationService: AuthorizationServiceProtocol?
-    private lazy var debouncingSessionUpdater = Debouncer(
-        delay: Constants.sessionCheckInterval,
-        callback: checkSession)
     
     // MARK: - Init
     
@@ -43,15 +40,12 @@ final class SessionTracker: SessionTrackerProtocol {
     
     func didLogin() {
         isLoggedIn = true
-        checkSession()
-        debouncingSessionUpdater.call()
     }
     
     func logOut(sessionTimeout: Bool) {
         guard isLoggedIn else { return }
         isLoggedIn = false
         goToPrelogin(sessionTimeout: sessionTimeout)
-        authorizationService?.logOut(success: nil, failure: nil)
     }
     
     // MARK: - Private methods
@@ -62,30 +56,10 @@ final class SessionTracker: SessionTrackerProtocol {
             selector: #selector(sessionDidTimeout),
             name: MutuallyNotification.sessionDidTimeout.name,
             object: nil)
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(checkSession),
-            name: UIApplication.willEnterForegroundNotification,
-            object: nil)
     }
     
     @objc private func sessionDidTimeout() {
         logOut(sessionTimeout: true)
-    }
-    
-    @objc private func checkSession() {
-        guard isLoggedIn else { return }
-        debouncingSessionUpdater.call()
-        authorizationService?.connect(
-            success: nil,
-            failure: { [weak self] (error) in
-                switch error {
-                case .serverError:
-                    self?.logOut(sessionTimeout: true)
-                default:
-                    return
-                }
-        })
     }
     
     private func goToPrelogin(sessionTimeout: Bool) {
