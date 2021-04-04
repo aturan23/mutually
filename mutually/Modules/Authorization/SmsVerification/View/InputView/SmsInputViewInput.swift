a190616afe60c8a06e7db7e939e8f67934551682
Turan Assylkhan <turan@strongte.am>
2021-04-04 13:04:34 +0600

    feat: show error def vals

--- mutually/Modules/Authorization/SmsVerification/View/InputView/SmsInputViewInput.swift
+++ mutually/Modules/Authorization/SmsVerification/View/InputView/SmsInputViewInput.swift
@@ -9,9 +9,15 @@ protocol SmsInputViewInput: class {
     func append(symbol: Character)
     func eraseLastSymbol()
     func disableInput()
-    func showError(message: String?)
+    func showError(message: String?, onHideAction: (() -> Void)?)
     func set(otp: String)
     func resetOtp()
+}
+
+extension SmsInputViewInput {
+    func showError(message: String? = nil, onHideAction: (() -> Void)? = nil) {
+        showError(message: message, onHideAction: onHideAction)
+    }
 }
 
 protocol SmsInputViewOutput: class {
