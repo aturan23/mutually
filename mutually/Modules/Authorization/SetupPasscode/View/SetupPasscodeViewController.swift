520ed5fae279dc2b91dd1fd6788fd69e7b39981e
Turan Assylkhan <turan@strongte.am>
2021-04-04 09:24:42 +0600

    feat: create pass view

--- mutually/Modules/Authorization/SetupPasscode/View/SetupPasscodeViewController.swift
+++ mutually/Modules/Authorization/SetupPasscode/View/SetupPasscodeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class SetupPasscodeViewController: BaseViewController, SetupPasscodeViewInput {
 
@@ -15,12 +16,30 @@ class SetupPasscodeViewController: BaseViewController, SetupPasscodeViewInput {
     // ------------------------------
 
     var output: SetupPasscodeViewOutput?
+    private var createViewCenterXConstraint: Constraint?
 
     // ------------------------------
     // MARK: - UI components
     // ------------------------------
     
     private let labelFactory = LabelFactory()
+    private lazy var passwordViewInput: SmsInputViewInput = passwordView
+    
+    private let createContainerView = UIView()
+    private lazy var createPasswordLabel = labelFactory.make(
+        withStyle: .paragraphSecondary,
+        text: "Придумайте 4х-значный пароль для последующего входа в приложение",
+        textColor: Color.textHighContrast)
+    private lazy var passwordView: SmsInputView = {
+        let view = SmsInputView(codeLength: 4)
+        view.output = self
+        return view
+    }()
+    
+    private lazy var repeatPasswordLabel = labelFactory.make(
+        withStyle: .paragraphSecondary,
+        text: "Повторите введенный ранее 4х-значный пароль для его подтверждения",
+        textColor: Color.textHighContrast)
     
     private lazy var keyboardView: PasswordKeyboardView = {
         let view = PasswordKeyboardView()
@@ -56,15 +75,28 @@ class SetupPasscodeViewController: BaseViewController, SetupPasscodeViewInput {
     }
 
     private func setupViewsHierarchy() {
-        [keyboardView].forEach(view.addSubview(_:))
+        [createPasswordLabel, passwordView].forEach(createContainerView.addSubview(_:))
+        [createContainerView, keyboardView].forEach(view.addSubview(_:))
     }
 
     private func setupConstraints() {
+        createPasswordLabel.snp.makeConstraints {
+            $0.top.width.equalToSuperview()
+        }
+        passwordView.snp.makeConstraints {
+            $0.top.equalTo(createPasswordLabel.snp.bottom).offset(LayoutGuidance.offsetSuperLarge)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+        createContainerView.snp.makeConstraints {
+            createViewCenterXConstraint = $0.centerX.equalToSuperview().constraint
+            $0.left.right.equalToSuperview().inset(LayoutGuidance.offsetSuperLarge)
+        }
+        
         var viewSafeAreaHeight: CGFloat = view.bounds.height
         if let windowInsets = UIApplication.shared.keyWindow?.safeAreaInsets {
             viewSafeAreaHeight -= windowInsets.top + windowInsets.bottom
         }
-        
         keyboardView.snp.makeConstraints {
             $0.height.equalTo(viewSafeAreaHeight / 2)
             $0.width.equalToSuperview()
@@ -74,18 +106,33 @@ class SetupPasscodeViewController: BaseViewController, SetupPasscodeViewInput {
 }
 
 // ------------------------------
+// MARK: - SmsInputViewOutput
+// ------------------------------
+
+extension SetupPasscodeViewController: SmsInputViewOutput {
+    
+    func didComplete(inputView: SmsInputView, code: String) {
+        if inputView == passwordView {
+//            output?.didCreate(passcode: password)
+        }// else if inputView == repeatPasswordView {
+//            output?.didTryToRepeat(passcode: password)
+//        }
+    }
+}
+
+// ------------------------------
 // MARK: - PasswordKeyboardViewOutput
 // ------------------------------
 
 extension SetupPasscodeViewController: PasswordKeyboardViewOutput {
+    
     func didPressButton(type: PasswordKeyboardButtonType) {
-//        switch type {
-//        case .digit(let value):
-//            passwordViewInput.append(symbol: value)
-//        case .backspace:
-//            passwordViewInput.eraseLastSymbol()
-//        case .biometry:
-//            break
-//        }
+        switch type {
+        case .digit(let value):
+            passwordViewInput.append(symbol: value)
+        case .backspace:
+            passwordViewInput.eraseLastSymbol()
+        case .biometry: break
+        }
     }
 }
