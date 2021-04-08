eb0a4d3f7457125c7621e6ee545d046277cc91f5
Turan Assylkhan <turan@strongte.am>
2021-04-08 08:15:00 +0600

    feat: whole number extension

---
+++ mutually/Source/Extensions/FloatExtension.swift
@@ -0,0 +1,14 @@
+//
+//  FloatExtension.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 08.04.2021.
+//
+
+import Foundation
+
+extension Float {
+    var wholeNumber: String {
+        return NSNumber(value: self).stringValue
+    }
+}
