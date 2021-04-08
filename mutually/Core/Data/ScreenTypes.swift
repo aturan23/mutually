d79b17ea02fe3c1f5f61b9b5f3627f9fbe992885
Turan Assylkhan <turan@strongte.am>
2021-04-08 08:28:09 +0600

    feat: screen types

---
+++ mutually/Core/Data/ScreenTypes.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenTypes.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 08.04.2021.
+//
+
+
+enum ScreenTypes: String, Codable {
+    case account
+    case newClient = "new_client"
+    case thanks = "thankYou_page"
+    case rejected
+}
