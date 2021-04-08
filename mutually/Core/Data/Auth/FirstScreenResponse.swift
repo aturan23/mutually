fc6595c679abbb839cf57b9f99e4ce915ee8753a
Turan Assylkhan <turan@strongte.am>
2021-04-08 09:00:30 +0600

    feat: firstScreenResponse

---
+++ mutually/Core/Data/Auth/FirstScreenResponse.swift
@@ -0,0 +1,12 @@
+//
+//  FirstScreenResponse.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 08.04.2021.
+//
+
+struct FirstScreenResponse: Codable {
+    let screen: ScreenTypes
+    let text: String?
+    let full: Bool
+}
