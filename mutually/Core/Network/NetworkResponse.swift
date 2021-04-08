5a41e26b1255591312a4344d606756a270631d93
Turan Assylkhan <turan@strongte.am>
2021-04-08 08:46:44 +0600

    feat: decode code

--- mutually/Core/Network/NetworkResponse.swift
+++ mutually/Core/Network/NetworkResponse.swift
@@ -9,10 +9,12 @@ import Foundation
 
 struct NetworkResponse: Codable {
     let result: Bool
+    let code: String?
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let resultSring = try container.decodeIfPresent(String.self, forKey: .result)
         result = resultSring == JSONResponseParameter.success.key
+        code = try container.decodeIfPresent(String.self, forKey: .code)
     }
 }
