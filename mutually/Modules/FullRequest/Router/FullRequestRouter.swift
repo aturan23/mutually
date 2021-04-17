146a7cae35c4513b30ff71ab201f14ad884bd343
Turan Assylkhan <turan@strongte.am>
2021-04-17 10:07:34 +0600

    feat: routing to camera

--- mutually/Modules/FullRequest/Router/FullRequestRouter.swift
+++ mutually/Modules/FullRequest/Router/FullRequestRouter.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
+typealias ImagePickerDelegate = UIImagePickerControllerDelegate & UINavigationControllerDelegate
+
 class FullRequestRouter: FullRequestRouterInput {
+    var alertFactory = AlertFactory()
 	weak var viewController: UIViewController?
+    private var imagePickerController = UIImagePickerController()
 
 	// ------------------------------
     // MARK: - FullRequestRouterInput
     // ------------------------------
+    
+    func routeToCamera(delegate: ImagePickerDelegate) {
+        imagePickerController.delegate = delegate
+        imagePickerController.sourceType = .camera
+//        let view = UIView(frame: .init(x: 100, y: 100, width: 100, height: 100))
+//        view.backgroundColor = .red
+//        imagePickerController.cameraOverlayView = view
+        viewController?.present(imagePickerController, animated: true)
+    }
 }
