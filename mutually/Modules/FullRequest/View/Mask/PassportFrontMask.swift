2a4cd09d43b9c4c0e793f04eff4ff3bf808f4da6
Turan Assylkhan <turan@strongte.am>
2021-04-17 12:46:33 +0600

    feat: passport fron view

---
+++ mutually/Modules/FullRequest/View/Mask/PassportFrontMask.swift
@@ -0,0 +1,47 @@
+//
+//  PassportFrontMask.swift
+//  mutually
+//
+//  Created by Turan Assylkhan on 17.04.2021.
+//
+
+import UIKit
+
+class PassportFrontMask: UIView {
+    
+    private let borderLineView = UIView()
+    private let photoLineView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        addSubview(borderLineView)
+        borderLineView.addSubview(photoLineView)
+        
+        borderLineView.layer.borderWidth = 1
+        borderLineView.layer.borderColor = UIColor.white.cgColor
+        borderLineView.layer.cornerRadius = 4
+        
+        photoLineView.layer.borderWidth = 1
+        photoLineView.layer.borderColor = UIColor.white.cgColor
+        photoLineView.layer.cornerRadius = 4
+        
+        borderLineView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(LayoutGuidance.offsetLarge)
+            $0.left.right.equalToSuperview().inset(LayoutGuidance.offsetSuperLarge * 1.25)
+        }
+        
+        photoLineView.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(LayoutGuidance.offsetSuperLarge * 2)
+            $0.height.equalTo(150)
+            $0.bottom.equalTo(-LayoutGuidance.offsetSuperLarge * 1.5)
+        }
+    }
+}
